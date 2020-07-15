8561b3a1a07055abcbd80fa06352ac5214b906cf
nickjii <nickykariss@gmail.com>
2020-07-15 15:03:21 +0300

    initial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    BRAINSTECH
+                    BRAINS
                 </div>
 
                 </div>
