6154b2e43f82b69a92af9605add36205996d8535
bernard <email@example.com>
2020-07-15 15:15:09 +0300

    initial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -86,10 +86,10 @@
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
-                    <a href="brainstech.herokuapp.com">brainstech</a>
+                    <a href="brainstech.herokuapp.com">brah</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
+                    <a href="https://nova.laravel.com">rerer</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
